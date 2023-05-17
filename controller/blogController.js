04999d195e4a8d70b9914c5ca880166c1fae6e02
Worapol Thongmug <worapolthongmug@MacBook-Pro-khxng-Worapol.local>
2023-05-17 20:05:21 +0700

    new commit

--- controller/blogController.js
+++ controller/blogController.js
@@ -2,6 +2,7 @@ const slugify = require('slugify')
 //นำ model blogs เข้ามาทำงานที่ blogController.js
 const Blogs = require("../model/blog")
 const blog = require('../model/blog')
+const {uuidv4} = require('uuid')
 
 exports.create = (req, res) => {
     //รับค่า request ที่ส่งมาเก็บลงใน object data
@@ -27,4 +28,24 @@ exports.getAllBlog = (req, res) => {
     }).catch(err => {
         console.log(err);
     })
+}
+
+exports.getSingleBlog = (req,res) =>{
+    const {slug} = req.params //เก็บค่า slug ที่ส่งมาพร้อม URL request ลงไป
+    if(!slug){
+        slug = uuidv4();
+    }
+    //ค้นหาบทความตาม slug ที่ส่งมา
+    Blogs.findOne({slug:slug}).exec().then(blog=>{
+        res.json(blog)
+    }).catch(err=>{
+        console.log(err);
+    })
+}
+
+exports.remove = (req,res) =>{
+    const {slug} = req.params
+    Blogs.findOneAndRemove({slug}).exec().then(blog=>{
+        res.json({error:"ลบบทความเรียบร้อยแล้ว"})
+    }).catch(err=>{console.log(err)})
 }
--- route/blog.js
+++ route/blog.js
@@ -1,10 +1,13 @@
 const express = require('express')
-const {create,tests, getAllBlog} = require('../controller/blogController')
+const {create, getAllBlog,getSingleBlog,remove} = require('../controller/blogController')
+
 //Create router
 const router = express.Router()
 
 //สร้าง Route
 router.post('/blog',create)
 router.get('/allBlog', getAllBlog)
+router.get('/singleBlog/:slug',getSingleBlog)
+router.delete('/singleBlog/:slug',remove)
 //export router 
 module.exports = router
